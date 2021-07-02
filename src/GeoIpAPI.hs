257d8705502e57d9ea3588431fb28a88a72c8ccc
Venkatesh Narayanan <bumrap@Venkateshs-MacBook-Air.local>
2021-07-02 12:11:53 +0530

    Telegram
    cleanup

--- src/GeoIpAPI.hs
+++ src/GeoIpAPI.hs
@@ -2,10 +2,13 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 
 module GeoIpAPI where
 import System.Environment
 import Data.Aeson as Q
+import Data.Aeson.Internal
+import Data.Aeson.Parser.Internal
 import Data.Text
 import Control.Applicative
 import Control.Monad
@@ -20,43 +23,27 @@ import qualified Data.Text as Data.ByteString.Char8
 import Data.Aeson
 import Data.Aeson.Types
 import GHC.Generics
-data Locdata = Locdata {
-  latitude  :: Float,
-  longitude :: Float,
-  _type :: Text,
-  name :: Text,
-  number :: Int,
-  postal_code :: Int,
-  street :: Text,
-  confidence :: Int,
-  region :: Text,
-  region_code :: Text,
-  county :: Text,
-  locality :: Text,
-  administrative_area :: Text,
-  neighbourhood :: Text,
-  country :: Text,
-  country_code :: Text,
-  continent :: Text,
-  label :: Text
-} deriving (Show, Generic)
+import Data.Char (isDigit)
 
+data Locdata = Locdata
+  { latitude :: Float,
+    longitude :: Float
+  }
+  deriving (Show, Generic)
+
+instance FromJSON Locdata
+instance ToJSON Locdata
 data ForwardGeoData = ForwardGeoData {
   _data :: [Locdata]
 } deriving (Show, Generic)
 
-instance FromJSON Locdata where
-    parseJSON = genericParseJSON defaultOptions { 
-      fieldLabelModifier = Prelude.drop 1 }
-
-keywordFieldLabelModifier "_type" = "type"
 keywordFieldLabelModifier "_data" = "data"
-
+keywordFieldLabelModifier id = id
 instance FromJSON ForwardGeoData where
   parseJSON = genericParseJSON defaultOptions {
     fieldLabelModifier = Prelude.drop 1
   }
-
+instance ToJSON ForwardGeoData
 data GeoIp = GeoIp {ip::String,
                     country_code::String,
                     country_name::String,
@@ -79,22 +66,45 @@ jsonGeoIpURL :: String
 jsonGeoIpURL = "https://freegeoip.app/json/"
 getGeoIpforThis ::  IO B.ByteString
 getGeoIpforThis = simpleHttp jsonGeoIpURL `X.catch` statusExceptionHandler
--- Gets LatLong for a place
 jsonPositionStackURL :: String
 jsonPositionStackURL = "http://api.positionstack.com/v1/forward?access_key="
+
+-- Gets LatLong only for a place only in INDIA (IN)
 getForwardGeoCodeforThis :: String -> IO B.ByteString
 getForwardGeoCodeforThis town = do
   key <- getEnv "API_POSITIONSTACK_KEY" -- set this in AWS Env. Variables
-  let urlCall = jsonPositionStackURL ++ key ++ "&query=" ++ town
+  let urlCall = jsonPositionStackURL ++ key ++ "&limit=1&fields=results.latitude,results.longitude&country=IN&query=" ++ town
   simpleHttp urlCall  `X.catch` statusExceptionHandler
-
+ 
+{--
 getLatLongforThis :: String -> IO Text
 getLatLongforThis town = do 
-  d <- (eitherDecode <$> getForwardGeoCodeforThis town) :: IO (Either String Locdata)
+  d <- (eitherDecode <$> getForwardGeoCodeforThis town) :: IO (Either String ForwardGeoData)
   case d of
     Left e ->  do 
                 f <- (eitherDecode <$> getGeoIpforThis) :: IO (Either String GeoIp)
                 case f of 
                   Left err -> return $ "Mystery Place" <> pack err
                   Right geoipstuff_backup -> return $ Data.ByteString.Char8.pack (show (latitude (geoipstuff_backup :: GeoIp))  ++ "," ++ show (longitude (geoipstuff_backup :: GeoIp)))
-    Right geoipstuff -> return $ Data.ByteString.Char8.pack (show (latitude (geoipstuff :: Locdata)) ++ "," ++ show  (longitude (geoipstuff :: Locdata)))
+    Right geoipstuff -> return $ Data.ByteString.Char8.pack 
+      (show (Prelude.head (_data (geoipstuff :: ForwardGeoData))))
+--}
+getLatLongforThis :: String -> IO Text
+getLatLongforThis town = do
+  d <- (eitherDecode <$> getForwardGeoCodeforThis town) :: IO (Either String ForwardGeoData)
+  case d of
+    Left e -> do
+      f <- (eitherDecode <$> getGeoIpforThis) :: IO (Either String GeoIp)
+      case f of
+        Left err -> return $ "Mystery Place" <> pack err
+        Right geoipstuff_backup -> return $ Data.ByteString.Char8.pack (show (latitude (geoipstuff_backup :: GeoIp)) ++ "," ++ show (longitude (geoipstuff_backup :: GeoIp)))
+    Right geoipstuff ->
+      return $
+        Data.ByteString.Char8.pack $ removeNonNumbers $
+          (show (Prelude.head (_data (geoipstuff :: ForwardGeoData))))
+
+isPeriodorCommaorDigit :: Char -> Bool
+isPeriodorCommaorDigit c = (c == '.') || (c == ',') || (isDigit c)
+
+removeNonNumbers :: String -> String
+removeNonNumbers = Prelude.filter isPeriodorCommaorDigit
