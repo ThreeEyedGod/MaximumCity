a825d94cbeaf16014213b3df473f9b683ebfe0df
Venkatesh Narayanan <bumrap@Venkateshs-MacBook-Air.local>
2021-07-13 23:04:48 +0530

    Telegram
    error handling

---
+++ .history/app/Main_20210713161727.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module Main where
+
+import Aws.Lambda
+import Lib
+import Telegram
+import Debug.Trace (trace)
+
+main :: IO ()
+main = do
+  -- tc <- getTelegramSettings
+  res <- getTelegramSettings
+  case res of 
+    Left msg -> trace ("Left Main =" runLambda (runError msg)
+    Right tc -> runLambda (run tc)
+  where
+    runError :: String -> LambdaOptions -> IO (Either String LambdaResult)
+    runError s opts = do
+      result <- badhandler s (decodeObj (eventObject opts)) (decodeObj (contextObject opts))
+      either (pure . Left . encodeObj) (pure . Right . LambdaResult . encodeObj) result
+    run :: TC -> LambdaOptions -> IO (Either String LambdaResult)
+    run tc opts = do
+      result <- handler tc (decodeObj (eventObject opts)) (decodeObj (contextObject opts))
+      either (pure . Left . encodeObj) (pure . Right . LambdaResult . encodeObj) result
---
+++ .history/app/Main_20210713211937.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module Main where
+
+import Aws.Lambda
+import Lib
+import Telegram
+import Debug.Trace (trace)
+
+main :: IO ()
+main = do
+  -- tc <- getTelegramSettings
+  res <- getTelegramSettings
+  case res of 
+    Left msg -> trace ("Left Main = " ++ show msg) $ runLambda (runError msg)
+    Right tc -> trace ("Right Main = " ++ "tc") $ runLambda (run tc)
+  where
+    runError :: String -> LambdaOptions -> IO (Either String LambdaResult)
+    runError
+      s LambdaOptions
+        { functionHandler = functionHandler,
+          contextObject = contextObject,
+          eventObject = eventObject,
+          executionUuid = executionUuid
+        } = do
+      case eventObject of
+      trace ("runError  = " ++ "s") $ (pure . Left . show) s
+      result <- badhandler s (decodeObj (eventObject opts)) (decodeObj (contextObject opts))
+      trace ("runError  = " ++ "s") $ either (pure . Left . encodeObj) (pure . Right . LambdaResult . encodeObj) result
+    run :: TC -> LambdaOptions -> IO (Either String LambdaResult)
+    run tc opts = do
+      result <- handler tc (decodeObj (eventObject opts)) (decodeObj (contextObject opts))
+      either (pure . Left . encodeObj) (pure . Right . LambdaResult . encodeObj) result
+
+check :: IO ()
+check 
+ LambdaOptions
+   { functionHandler = functionHandler,
+     contextObject = contextObject,
+     eventObject = eventObject,
+     executionUuid = executionUuid
+   }
+  let x :: LambdaOptions 
+  main x
