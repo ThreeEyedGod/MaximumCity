a8194a5ae378d3eb6113d3eab2814673c03eb1a7
Venkatesh Narayanan <bumrap@Venkateshs-MacBook-Air.local>
2021-08-24 21:45:07 +0530

    Attoparsec based
    PDF parsing+telegram integration

---
+++ .history/src/WaterLevelLakes_20210824121856.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module WaterLevelLakes where
+
+import WaterLevelHeaders
+import Pdf.Document
+import JSONHelper
+
+import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString as DB
+import Data.ByteString.UTF8 as BSU  
+import Control.Exception as X
+
+import Network.HTTP.Conduit (simpleHttp)
+
+import Data.Word
+import Data.Time
+
+--import qualified Data.Attoparsec.Text.Internal as API
+--import Data.Attoparsec.ByteString.Char8.Parser
+
+import Control.Applicative
+-- We import ByteString qualified because the function
+-- 'Data.ByteString.readFile' would clash with
+-- 'Prelude.readFile'.
+--import qualified Data.ByteString as B
+import Data.Text.Encoding as TSE
+
+wlURL :: String
+wlURL = "https://d3suziiw6thyiv.cloudfront.net/reports/storage-comparison/standard/pdf/view?MenuID=1317"
+getPdfAtThisURL ::  IO B.ByteString
+getPdfAtThisURL = simpleHttp wlURL `X.catch` exceptionHandler
+
+getPageofPDF :: Int -> IO T.Text 
+getPageofPDF  = do
+      pdfByteString <- getPdfAtThisURL
+      pdf <- fromBytes $ B.toStrict pdfByteString
+      doc <- document pdf
+
+      maybe_info <- documentInfo doc
+      title <- case maybe_info of
+          Nothing -> return Nothing
+          Just info -> infoTitle info
+
+      root <- documentCatalog doc >>= catalogPageNode
+      total <- pageNodeNKids root
+
+      return (root, total, title)
+
+      firstPage <- pageNodePageByNum root 0
+      eigthPage <- pageNodePageByNum root 7 
+      txt <- pageExtractText eigthPage
+
+      pure txt
+ 
---
+++ .history/src/WaterLevelLakes_20210824130739.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module WaterLevelLakes where
+
+import WaterLevelHeaders
+import Pdf.Document
+import Data.Attoparsec.ByteString as Att
+import Data.Attoparsec.ByteString.Char8 (skipSpace, isDigit_w8, manyTill, isHorizontalSpace, endOfLine, char, decimal, space, double, letter_ascii)
+
+import JSONHelper
+import ParserHelper
+
+import qualified Data.Text as T
+import Data.Text.Encoding as TSE
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString as DB
+import Data.ByteString.UTF8 as BSU  
+import Control.Exception as X
+
+import Network.HTTP.Conduit (simpleHttp)
+
+import Data.Word
+import Data.Time
+
+--import qualified Data.Attoparsec.Text.Internal as API
+--import Data.Attoparsec.ByteString.Char8.Parser
+
+import Control.Applicative
+-- We import ByteString qualified because the function
+-- 'Data.ByteString.readFile' would clash with
+-- 'Prelude.readFile'.
+--import qualified Data.ByteString as B
+import Data.Text.Encoding as TSE
+
+wlURL :: String
+wlURL = "https://d3suziiw6thyiv.cloudfront.net/reports/storage-comparison/standard/pdf/view?MenuID=1317"
+getPdfAtThisURL ::  String -> IO B.ByteString
+getPdfAtThisURL url = simpleHttp url `X.catch` exceptionHandler
+
+getPageofPDFat :: String -> Int -> IO T.Text 
+getPageofPDFat url pageNum = do
+      pdfByteString <- getPdfAtThisURL url
+      pdf <- fromBytes $ B.toStrict pdfByteString
+      doc <- document pdf
+
+      maybe_info <- documentInfo doc
+      title <- case maybe_info of
+          Nothing -> return Nothing
+          Just info -> infoTitle info
+
+      root <- documentCatalog doc >>= catalogPageNode
+      total <- pageNodeNKids root
+
+      return (root, total, title)
+
+      firstPage <- pageNodePageByNum root 0
+      page <- pageNodePageByNum root pageNum
+      txt <- pageExtractText page
+
+      pure txt
+ 
+getWaterLakeLevelPDFData :: Int -> IO ByteString
+getWaterLakeLevelPDFData pageNum = do 
+    x <- getPageofPDFat wlURL pageNum
+    pure $ TSE.encodeUtf8 x
+
+getWaterLakeLevelParsedData :: Int -> IO ByteString
+getWaterLakeLevelParsedData pageN = do 
+    x <- getWaterLakeLevelPDFData n
+    parseOnly $ page8PageParser x
