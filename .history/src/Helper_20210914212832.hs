7e8ec1a077fa939c258d348911679fff267ce79c
Venkatesh Narayanan <bumrap@Venkateshs-MacBook-Air.local>
2021-09-19 11:00:47 +0530

    Attoparsec based
    PDF page8-9

---
+++ .history/src/Helper_20210914212832.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Helper where
+import Control.Applicative
+import Control.Exception as X
+import Control.Exception (IOException, handle, throw, catch)
+import Control.Monad
+import Data.Text
+import GHC.Generics
+import Prelude
+import Data.Bifunctor (first)
+import qualified Data.ByteString.Lazy as B
+import System.IO.Error (isDoesNotExistError, tryIOError)
+import System.Environment (getEnv)
+import Numeric
+import Data.Char (isDigit)
+
+maybeHead :: [a] -> Maybe a
+maybeHead []    = Nothing
+maybeHead (x:_) = Just x
+
+isPeriodorCommaorDigit :: Char -> Bool
+isPeriodorCommaorDigit c = (c == '.') || (c == ',') || (isDigit c)
+
+removeNonNumbers :: String -> String
+removeNonNumbers = Prelude.filter isPeriodorCommaorDigit
+
+-- concatenate strings to make a string
+catSS :: String -> String -> String
+catSS r s  = r ++ s 
+
+-- concatenate string and a float to make a string with float precision 2
+catSF :: String -> Float -> String
+catSF r f = r ++ showFFloat (Just 2) f ""
+
+-- concatenate string and int to make a string
+catSI :: String -> Int -> String
+catSI r i = r ++ show i
+
+fahrenheitToCelsius :: Float -> Float
+fahrenheitToCelsius f = (5 / 9) * (f - 32)
+
+-- deriving Eq because in SpecHelper tests we are testing for equality 
+data EnvError
+  = MissingEnvError String
+  | EmptyKeyError String
+  | SomeIOError IOError
+  deriving (Eq, Show)
+
+badEnv :: String -> IOException -> IO (Either EnvError String)
+badEnv env ex
+      | Prelude.null env = return $ Left $ EmptyKeyError "Environment key not given "
+      | isDoesNotExistError ex = return $ Left $ (MissingEnvError env)
+      | otherwise = return $ Left $ SomeIOError ex
+
+-- handle is basically a guard  - a better catch !
+-- reminder: 
+getKey :: String -> IO (Either EnvError String)
+getKey env = handle (badEnv env) $ Right <$> (getEnv env)
+
+orDieonNothing :: Maybe a -> String -> Either String a
+Just a  `orDieonNothing` _      = return a
+Nothing `orDieonNothing` string = Left string
---
+++ .history/src/Weather_20210914173954.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Weather (getTownNameWeatherFromIp, getTownNameWeatherFromTown, getWeather, PreprocessedHeaders) where
+import qualified Data.ByteString.Lazy.Char8 as LB
+import qualified Data.Text as Data.ByteString.Char8
+import qualified Data.Text as T
+import GHC.Integer.Logarithms ()
+import OpenWeatherAPI
+import PirateWeatherAPI
+import IP2Location
+import WaterLevelLakes
+import WaterLevels.MH.Core.WaterLevelHeaders
+import Data.Text.Encoding as TSE
+import Data.Text.Lazy.Encoding as TLE
+import Data.ByteString.UTF8 as BSU 
+import Data.Maybe
+
+type PreprocessedHeaders = LB.ByteString 
+type PlaceName = T.Text 
+type TheWeatherThere = T.Text
+
+getTownNameWeatherFromIp :: PreprocessedHeaders -> IO TheWeatherThere
+getTownNameWeatherFromIp headers = do
+  town <- extractXForwardedForHeader headers
+  if "Fail" `T.isPrefixOf` town
+    then do
+      return $ "Fail"
+    else do
+      getTownNameWeatherFromTown town
+
+_helperLivePercent :: (Maybe Region, Maybe PercentLiveStorage) -> String
+_helperLivePercent ( Nothing , _ ) = " % livelake level Not Available "
+_helperLivePercent (_ , Nothing) = " % livelake level Not Available "
+_helperLivePercent (Just a, Just b) = " % livelake level at " ++ (BSU.toString a) ++ " is " ++ (BSU.toString $ (percent_Today b))
+
+_handlePirateResponse :: T.Text -> PlaceName -> String -> IO TheWeatherThere
+_handlePirateResponse "Fail" town wll = do
+    weather2 <- OpenWeatherAPI.getWeatherForTown $ Data.ByteString.Char8.unpack $ town
+    let tw = (Data.ByteString.Char8.unpack town ++ " is currently " ++ Data.ByteString.Char8.unpack weather2 ++ wll )
+    return $ Data.ByteString.Char8.pack tw
+_handlePirateResponse weather1 town wll =  do
+    let tw = (Data.ByteString.Char8.unpack town ++ " is currently " ++ Data.ByteString.Char8.unpack weather1 ++ wll )
+    return $ Data.ByteString.Char8.pack tw
+
+getTownNameWeatherFromTown :: PlaceName -> IO TheWeatherThere
+getTownNameWeatherFromTown town = do
+  (region, lakelevel) <- getWaterLakeLevelForPlace_LiveToday_wrtStorage town
+  let wll = _helperLivePercent (region,lakelevel)
+  weather1 <- PirateWeatherAPI.getWeatherForTown $ Data.ByteString.Char8.unpack $ town
+  if "Fail" `T.isPrefixOf` weather1
+  then do                 
+    weather2 <- OpenWeatherAPI.getWeatherForTown $ Data.ByteString.Char8.unpack $ town
+    let tw = (Data.ByteString.Char8.unpack town ++ " is currently " ++ Data.ByteString.Char8.unpack weather2 ++ wll )
+    return $ Data.ByteString.Char8.pack tw
+  else do  
+    let tw = (Data.ByteString.Char8.unpack town ++ " is currently " ++ Data.ByteString.Char8.unpack weather1 ++ wll )
+    return $ Data.ByteString.Char8.pack tw
+
+getWeather :: Maybe PreprocessedHeaders -> Maybe PlaceName -> IO TheWeatherThere
+getWeather (Just p) Nothing = getTownNameWeatherFromIp p
+getWeather _ (Just pl) = getTownNameWeatherFromTown pl 
