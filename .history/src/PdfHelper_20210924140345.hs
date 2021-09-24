88265602caeb424b2320938fbffbfe6d20fe1d7f
Venkatesh Narayanan <bumrap@Venkateshs-MacBook-Air.local>
2021-09-24 15:58:05 +0530

    Refactoring Error H
    Cleanup

---
+++ .history/src/PdfHelper_20210924140345.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+module PdfHelper where
+
+import Pdf.Document
+import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as B
+import Control.Exception as X
+import Network.HTTP.Conduit (simpleHttp)
+import JSONHelper
+
+type FirstPage = Int
+type LastPage = Int
+
+getPdfAtThisURL ::  String -> IO B.ByteString
+getPdfAtThisURL url = simpleHttp url `X.catch` exceptionHandler
+
+
+readPagesPdf :: PageNode -> Int -> Int -> IO T.Text 
+readPagesPdf pn a b  | a > b = pure ""
+                     | otherwise = do 
+                            page <- pageNodePageByNum pn a
+                            txt <- pageExtractText page
+                            nxt <- readPagesPdf pn (a+1) b
+                            pure $ mconcat [txt, nxt]
+
+getPDFProperties :: String -> IO (PageNode, Int, Maybe T.Text)
+getPDFProperties url  = do
+      pdfByteString <- getPdfAtThisURL url
+      pdf <- fromBytes $ B.toStrict pdfByteString
+      doc <- document pdf
+
+      maybe_info <- documentInfo doc
+      title <- case maybe_info of
+          Nothing -> return Nothing
+          Just info -> infoTitle info
+
+      root <- documentCatalog doc >>= catalogPageNode
+      total <- pageNodeNKids root
+
+      return (root, total, title)
+
+getPagesofPDFfromTo :: String -> FirstPage -> Int -> IO T.Text 
+getPagesofPDFfromTo url pageBeg pageEnd = do
+      (root, total, title) <- getPDFProperties url
+      extract <- readPagesPdf root pageBeg pageEnd
+      pure extract
