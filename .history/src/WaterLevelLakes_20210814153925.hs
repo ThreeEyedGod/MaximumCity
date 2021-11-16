437f82e04d51a8ded8530e6172c1a3f083b7f74c
ThreeEyedGod <venkatesh.narayanan@live.in>
2021-11-16 12:10:09 +0530

    Delete .history/src directory

--- .history/src/WaterLevelLakes_20210814153925.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# OPTIONS_GHC -Wno-orphans #-}
-
-module WaterLevelLakes where
