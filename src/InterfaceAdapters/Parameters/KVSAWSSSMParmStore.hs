a51f2e3b5f5ee5b74ae0869264ac21d48ca50793
ThreeEyedGod <venkatesh.narayanan@live.in>
2022-03-23 19:51:28 +0530

    refactor8

--- src/InterfaceAdapters/Parameters/KVSAWSSSMParmStore.hs
+++ src/InterfaceAdapters/Parameters/KVSAWSSSMParmStore.hs
@@ -49,7 +49,8 @@ getAction key = do
   (value, version) <- doGetParameter (ParameterName "/AAA/BBB") ssmSession
   logMessage $ T.unpack value
   -- the next line is a problem 
-  return $ (decode $ (BL.fromChunks . return . T.encodeUtf8 $ value)) 
+  -- return $ (decode $ (BL.fromChunks . return . T.encodeUtf8 $ value)) 
+  return $ (decode $ (BL.fromStrict . T.encodeUtf8 $ value)) 
 
 
 -- | store persistent entity of type a and identified by id to the filesystem
