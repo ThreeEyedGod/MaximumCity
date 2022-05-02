2d9933ffb7fa3109f8c250a3e7fa6f200561ece9
ThreeEyedGod <venkatesh.narayanan@live.in>
2022-05-02 21:40:24 +0530

    aws ssm parm

--- src/InterfaceAdapters/Parameters/Types.hs
+++ src/InterfaceAdapters/Parameters/Types.hs
@@ -6,8 +6,5 @@ import Data.String                      (IsString)
 import Data.Text (Text)
 import Data.Data (Typeable)
 
---newtype ParameterName = ParameterName Text deriving (IsString, Eq, Ord, Show, Typeable)
---newtype ParameterValue = ParameterValue Text deriving (IsString, Eq, Ord, Show, Typeable)
-
 type ParameterName = Text 
 type ParameterValue = Text 
--- src/InterfaceAdapters/Preferences.hs
+++ src/InterfaceAdapters/Preferences.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE TypeInType, StandaloneDeriving #-}
-{-# LANGUAGE TemplateHaskell, LambdaCase, BlockArguments, GADTs
+{-# LANGUAGE TemplateHaskell, LambdaCase, BlockArguments, GADTs 
            , FlexibleContexts, TypeOperators, DataKinds, PolyKinds, ScopedTypeVariables #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module InterfaceAdapters.Preferences where
 
@@ -12,48 +14,56 @@ import InterfaceAdapters.Parameters.KVSAWSSSMParmStore
 import InterfaceAdapters.Utils.Helper
 import Data.Text (Text)
 import Data.Text as T
+import Data.Text.Encoding as T (encodeUtf8)
 import InterfaceAdapters.Parameters.Types
 import Amazonka.SSM (ParameterTier(ParameterTier_Advanced))
 import Data.Data (cast)
+import Amazonka (FromJSON, ToJSON, eitherDecode)
+import Amazonka.Prelude (Generic)
+import qualified Data.ByteString.Lazy as TL
+import Data.Aeson (eitherDecodeStrict)
 
 data Preferences = Preferences {
   userdata :: Agdata
 , usersize :: Datasize
 , usertimespan :: Timespan
-}
-data Agdata = Weather | WaterLevels | WeatherWaterLevels | Monsoon | All deriving (Show, Eq)
-data Datasize = Mini | Standard | Detailed deriving (Show, Eq)
-data Timespan = RightNow | Alerts | NearForecast | LongRange deriving (Show, Eq)
+} deriving (Show, Generic, Eq)
+
+instance FromJSON Preferences
+instance ToJSON Preferences
+
+data Agdata = Weather | WaterLevels | WeatherWaterLevels | Monsoon | All deriving (Show, Eq, Generic, FromJSON, ToJSON)
+data Datasize = Mini | Standard | Detailed deriving (Show, Eq, Generic, FromJSON, ToJSON)
+data Timespan = RightNow | Alerts | NearForecast | LongRange deriving (Show, Eq, Generic, FromJSON, ToJSON)
+
 
 runGetParm :: ParameterName -> IO Text
-runGetParm key = do 
+runGetParm key = do
   getKvs key
   & runKvsAsAWSSSMParmStore
   & runM
 
 runSetParm :: ParameterName -> ParameterValue -> IO ()
-runSetParm key val = do 
-  insertKvs key val 
+runSetParm key val = do
+  insertKvs key val
   & runKvsAsAWSSSMParmStore
   & runM
 
 textToParameterType :: Text -> Maybe ParameterName
 textToParameterType t  = cast t :: Maybe ParameterName
 
+setPreferences :: Text -> Text -> IO ()
+setPreferences = runSetParm
+
 getPreferences :: Text -> IO Preferences
-getPreferences user_id = do 
-  let v = "{\"userdata\":\"Weather\", \"usersize\": \"Mini\",\"usertimespan\":\"NearForecast\"}" :: ParameterValue
-  logMessage $ T.unpack user_id  
-  let puser_id  = textToParameterType user_id
-  case puser_id of
-    Nothing -> error "no user_id in incoming telegram"
-    Just p  -> do 
-      runSetParm p v
-      logMessage "runSetParm done "
-      x <- runGetParm p
-      logMessage "After runGetParm "
-      logMessage (show x)
-  return Preferences {userdata = Weather, usersize = Mini, usertimespan = NearForecast}
+getPreferences user_id = do
+  x <- runGetParm user_id
+  let y = eitherDecodeStrict $ T.encodeUtf8 x :: Either String Preferences
+  case y of
+    Left err -> do
+        logMessage "No Preferences for user id"
+        return Preferences {userdata = Weather, usersize = Mini, usertimespan = NearForecast}
+    Right prefs -> return prefs
 
 data MyPreferences m a where
   ReadPrefs :: String -> MyPreferences m Preferences
@@ -65,7 +75,7 @@ runprefsToIO :: Member (Embed IO) r => Sem (MyPreferences ': r) a -> Sem r a
 runprefsToIO = interpret (\(ReadPrefs userId) -> embed (getPreferences $ T.pack userId))
 
 runGetPrefs :: String -> IO Preferences
-runGetPrefs userid = do 
+runGetPrefs userid = do
   readPrefs userid
   & runprefsToIO
   & runM
