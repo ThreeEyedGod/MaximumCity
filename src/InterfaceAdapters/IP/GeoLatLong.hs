38862a1674b2a28229f8b49e0c4562de05a5d8ae
Venkatesh Narayanan <havana@Venkateshs-Air.Dlink>
2023-03-05 07:56:01 +0530

    shortcircuit3

--- src/InterfaceAdapters/IP/GeoLatLong.hs
+++ src/InterfaceAdapters/IP/GeoLatLong.hs
@@ -1,54 +1,42 @@
 
-{-# LANGUAGE OverloadedStrings, DeriveGeneric, DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-@ LIQUID "--skip-module" @-}
 
 module InterfaceAdapters.IP.GeoLatLong (getLatLongforThis) where
-import Data.Aeson
-    ( 
-      FromJSON,
-      ToJSON,
-      eitherDecode
-       )
+import Data.Aeson (eitherDecode)
 import Data.Text as T
 import Control.Applicative
 import Control.Monad
-import qualified Data.ByteString.Lazy as B
-
 import Prelude
 import qualified Data.Text as Data.ByteString.Char8
 import InterfaceAdapters.Utils.Helper
-import Data.Text.Lazy as TL
-import Data.Text.Lazy.Encoding as TLE
-import Data.Char as DC
 import InterfaceAdapters.IP.GeoCodeOpenCage (getOpenCageForwardGeoCodefor, OpenCageForwardGeoData (..), OpenCageResultData(..), OpenCageLocdata(..))
 import InterfaceAdapters.IP.GeoIpAPI (getPositionStackForwardGeoCodefor, ForwardGeoData (..))
-import Data.Either ( fromRight, isLeft, isRight )
-import InterfaceAdapters.Utils.ShortCircuit as SC
+import InterfaceAdapters.Utils.ShortCircuit (orM)
 
 getLatLongforThis :: String -> IO T.Text
-getLatLongforThis town = getLLData town >>= geoDataToText
+getLatLongforThis town = getLLData town >>= geoDataJSONToText
 
 getLLData :: String ->  IO (Either String (Either ForwardGeoData OpenCageForwardGeoData))
-getLLData s = SC.orM (fGDFromPlacePre s >>= fGDFromPlace) (oCFGDFromPlacePre s >>= oCFGDFromPlace)
+getLLData s = orM (getPositionStackJSON s >>= makeNestedEitherPositionStack) (getOpenCageJSON s >>= makeNestedEitherOpenCage) -- lazy evaluate first failover to second
 
-fGDFromPlacePre :: String -> IO (Either String ForwardGeoData)
-fGDFromPlacePre s = eitherDecode <$> getPositionStackForwardGeoCodefor s
+getPositionStackJSON :: String -> IO (Either String ForwardGeoData)
+getPositionStackJSON s = eitherDecode <$> getPositionStackForwardGeoCodefor s
 
-fGDFromPlace :: Either String ForwardGeoData -> IO (Either String (Either ForwardGeoData OpenCageForwardGeoData))
-fGDFromPlace (Left x)  = pure $ Left x
-fGDFromPlace (Right y) = pure $ Right $ Left y 
+makeNestedEitherPositionStack :: Either String ForwardGeoData -> IO (Either String (Either ForwardGeoData OpenCageForwardGeoData))
+makeNestedEitherPositionStack (Left x)  = pure $ Left x
+makeNestedEitherPositionStack (Right y) = pure $ Right $ Left y 
 
-oCFGDFromPlacePre :: String -> IO (Either String OpenCageForwardGeoData)
-oCFGDFromPlacePre s = eitherDecode <$> getOpenCageForwardGeoCodefor s
+getOpenCageJSON :: String -> IO (Either String OpenCageForwardGeoData)
+getOpenCageJSON s = eitherDecode <$> getOpenCageForwardGeoCodefor s
 
-oCFGDFromPlace :: Either String OpenCageForwardGeoData -> IO (Either String (Either ForwardGeoData OpenCageForwardGeoData))
-oCFGDFromPlace (Left x)  = pure $ Left x
-oCFGDFromPlace (Right y) = pure $ Right $ Right y 
+makeNestedEitherOpenCage :: Either String OpenCageForwardGeoData -> IO (Either String (Either ForwardGeoData OpenCageForwardGeoData))
+makeNestedEitherOpenCage (Left x)  = pure $ Left x
+makeNestedEitherOpenCage (Right y) = pure $ Right $ Right y 
 
-geoDataToText :: Either String (Either ForwardGeoData OpenCageForwardGeoData) -> IO T.Text
-geoDataToText (Left f)          = return $ T.pack f
-geoDataToText (Right (Left f))  = return $ Data.ByteString.Char8.pack $ removeNonNumbers (show (Prelude.head (_data (f :: ForwardGeoData))))
-geoDataToText (Right (Right f)) = return $ Data.ByteString.Char8.pack (show (lat (geometry (Prelude.head (results (f :: OpenCageForwardGeoData))))) ++ "," ++ show (lng (geometry (Prelude.head (results (f :: OpenCageForwardGeoData))))))
+geoDataJSONToText :: Either String (Either ForwardGeoData OpenCageForwardGeoData) -> IO T.Text
+geoDataJSONToText (Left f)          = return $ T.pack f
+geoDataJSONToText (Right (Left f))  = return $ Data.ByteString.Char8.pack $ removeNonNumbers (show (Prelude.head (_data (f :: ForwardGeoData))))
+geoDataJSONToText (Right (Right f)) = return $ Data.ByteString.Char8.pack (show (lat (geometry (Prelude.head (results (f :: OpenCageForwardGeoData))))) ++ "," ++ show (lng (geometry (Prelude.head (results (f :: OpenCageForwardGeoData))))))
